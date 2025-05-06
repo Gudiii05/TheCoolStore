e95edf33d4d617514f483f56d6227957b2f54586
samuel-tapia <samueltapgar@gmail.com>
2025-05-06 16:29:37 +0200

    Arreglada funcion fn_password_match, ahora si el usuario no existe tambien devuelve false

--- backend/functions/fn_password_match.py
+++ backend/functions/fn_password_match.py
@@ -12,7 +12,7 @@ def fn_password_match(user, passwd):
     cur.execute("SELECT * FROM users WHERE userID = ?", (user,)) #comprueba si la var username, esta en la tabla usuarios
     result = cur.fetchone()
     con.close()
-    if result[3] == passwd:
+    if result is not None and result[3] == passwd:
         return True
     else:
         return False
@@ -23,4 +23,5 @@ print(fn_password_match("ahmed.khalil@example.com", "hash1234"))  # Return True
 
 print(fn_password_match("ahmed.khalil@example.com", "")) # Return False
 
+print(fn_password_match("", "hola123")) # Return Falses
 # --------------------------------------------
